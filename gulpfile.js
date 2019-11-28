43109f0688c549f1f9a73efe59263d0d0e2c8297
Neil Swart <neil@webstreet.co.za>
2019-11-28 13:21:51 +0200

    added babel for es6 decompite

--- gulpfile.js
+++ gulpfile.js
@@ -1,92 +1,98 @@
-var config      = require('./gulp_config.js');
-var gulp        = require('gulp');
-var sass		= require('gulp-sass');
-var browserSync = require('browser-sync').create();
-var plumber     = require('gulp-plumber');
-var notify      = require('gulp-notify');
-var gutil       = require('gulp-util');
+const config = require('./gulp_config.js');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+const plumber = require('gulp-plumber');
+const notify = require('gulp-notify');
+const gutil = require('gulp-util');
+const babel = require('gulp-babel');
+const minify = require('gulp-babel-minify');
 
-var reload      = browserSync.reload;
-var siteName    = config.site;
-var themeName   = config.theme;
-var themePath   = 'wp-content/themes/' + themeName;
-var sassPath    = themePath + '/assets/sass';
-var scriptsPath = themePath + '/assets/js';
-var stylesPath  = themePath + '/assets/css';
+const reload = browserSync.reload;
+const path = {
+    src: {
+        sass: 'assets/src/sass',
+        script: 'assets/src/js',
+    },
+    dist: {
+        script: 'assets/dist/js',
+        style: 'assets/dist/css',
+    },
+    theme: 'wp-content/themes/' + config.theme
+};
 
-// Start browserSync
 gulp.task('browser-sync', function () {
     browserSync.init({
-        proxy: 'http://' + config.host + '/' + config.dir + '/' + siteName
+        proxy: 'http://' + config.host + '/' + config.dir + '/' + config.site
     });
 });
 
-gulp.task('process_sass', function(){
-	gulp.src(sassPath + '/styles.scss')
-    .pipe(plumber({errorHandler: function (err) {
-        notify.onError({
-            title: "Gulp error in " + err.plugin,
-            message: err.toString()
-        })(err);
-        gutil.beep();
-    }}))
-	.pipe(sass({
-        outputStyle: 'nested'
-    }))
-	.pipe(gulp.dest(stylesPath));
+gulp.task('process_sass', () => {
+    gulp.src(path.src.sass + '/styles.scss')
+        .pipe(plumber({
+            errorHandler: function (err) {
+                notify.onError({
+                    title: "Gulp error in " + err.plugin,
+                    message: err.toString()
+                })(err);
+                gutil.beep();
+            }
+        }))
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(gulp.dest(path.dist.style));
+
+    gulp.src(path.src.sass + '/admin_styles.scss')
+        .pipe(plumber({
+            errorHandler: function (err) {
+                notify.onError({
+                    title: "Gulp error in " + err.plugin,
+                    message: err.toString()
+                })(err);
+                gutil.beep();
+            }
+        }))
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(gulp.dest(path.dist.style));
 });
 
-gulp.task('sass_compress', function(){
-	gulp.src(sassPath + '/styles.scss')
-    .pipe(plumber({errorHandler: function (err) {
-        notify.onError({
-            title: "Gulp error in " + err.plugin,
-            message: err.toString()
-        })(err);
-        gutil.beep();
-    }}))
-	.pipe(sass({
-        outputStyle: 'compressed'
-    }))
-	.pipe(gulp.dest(stylesPath));
-});
-
-gulp.task('sass_compress_admin', function(){
-	gulp.src(sassPath + '/admin_styles.scss')
-    .pipe(plumber({errorHandler: function (err) {
-        notify.onError({
-            title: "Gulp error in " + err.plugin,
-            message: err.toString()
-        })(err);
-        gutil.beep();
-    }}))
-	.pipe(sass({
-        outputStyle: 'compressed'
-    }))
-	.pipe(gulp.dest(stylesPath));
+gulp.task('scripts_compile', () => {
+    gulp.src(path.src.script + '/scripts.js')
+        .pipe(babel({
+            presets: ['@babel/env'],
+        }))
+        .pipe(minify({
+            mangle: {
+                keepClassName: true
+            }
+        }))
+        .pipe(gulp.dest(path.dist.script))
 });
 
 gulp.task('styles', function () {
-    gulp.src(stylesPath + '/**/*.css')
-    .pipe(browserSync.stream());
+    gulp.src(path.dist.style + '/**/*.css')
+        .pipe(browserSync.stream());
 });
 
 gulp.task('scripts', function () {
-    gulp.src(scriptsPath + '/**/*.js')
-    .pipe(browserSync.stream());
+    gulp.src(path.src.script + '/**/*.js')
+        .pipe(browserSync.stream());
 });
 
 gulp.task('php', function () {
-    gulp.src(themePath + '/**/*.php')
-    .pipe(browserSync.stream());
+    gulp.src('./**/*.php')
+        .pipe(browserSync.stream());
 });
 
 gulp.task('watch', ['browser-sync', 'styles', 'php'], function () {
-    gulp.watch(scriptsPath + '/**/*.js', ['scripts']);
-    gulp.watch(stylesPath + '/**/*.css', ['styles']);
-    gulp.watch(sassPath + '/**/*.scss', ['sass_compress']);
-    // gulp.watch(sassPath + '/**/*.scss', ['sass_compress_admin']);
-    gulp.watch(themePath + '/**/*.php', ['php']);
+    gulp.watch(path.src.script + '/**/*.js', ['scripts']);
+    gulp.watch(path.dist.style + '/**/*.css', ['styles']);
+    gulp.watch(path.src.sass + '/**/*.scss', ['process_sass']);
+    gulp.watch(path.src.script + '/**/*.js', ['scripts_compile']);
+    gulp.watch('./**/*.php', ['php']);
 });
 
-gulp.task('default', ['browser-sync', 'scripts', 'styles', 'php', 'sass_compress', 'watch']);
+gulp.task('default', ['browser-sync', 'scripts', 'styles', 'php', 'process_sass', 'scripts_compile', 'watch']);
